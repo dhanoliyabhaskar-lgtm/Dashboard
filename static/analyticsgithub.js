e298f258353da419b23d431c76998a0d4d384671
Bhaskar Dhanoliya <dhanoliyabhaskar@gmail.com>
2026-07-09 15:33:53 +0530

    Adding the Java Script of a pug page.

---
+++ static/analyticsgithub.js
@@ -0,0 +1,133 @@
+// chart1 css
+
+const options = {
+    series: [{
+        name: "Commits",
+        data: [8, 3, 5, 12, 15, 21, 23, 31, 16, 6, 8, 36, 24, 29, 18, 22, 12, 19, 16, 31, 25, 5, 11, 8, 13, 20, 23, 13, 10, 17]
+    }],
+
+    chart: {
+        type: "area",
+        height: 300,
+    },
+
+    xaxis: {
+        categories: [
+            "Jun 1", "Jun 2", "Jun 3", "Jun 4", "Jun 5",
+            "Jun 6", "Jun 7", "Jun 8", "Jun 9", "Jun 10",
+            "Jun 11", "Jun 12", "Jun 13", "Jun 14", "Jun 15",
+            "Jun 16", "Jun 17", "Jun 18", "Jun 19", "Jun 20",
+            "Jun 21", "Jun 22", "Jun 23", "Jun 24", "Jun 25",
+            "Jun 26", "Jun 27", "Jun 28", "Jun 29", "Jun 30"
+        ],
+
+        labels: {
+            rotate: 0,
+            hideOverlappingLabels: true,
+            style: {
+                fontSize: "11px"
+            }
+        },
+
+        axisBorder: {
+            show: false
+        },
+
+    },
+
+    title: {
+        text: "Commit Activity",
+        align: "left",
+        style: {
+            fontSize: "15px",
+            color: 'black'
+        }
+    },
+
+    dataLabels: {
+        enabled: false,
+    }
+};
+
+var chartElement = document.querySelector("#commitChart");
+var chart1 = new ApexCharts(chartElement, options);
+chart1.render();
+
+
+
+
+
+// chart2 css
+
+var donut = {
+    chart: {
+        type: 'donut',
+        height: '290',
+        toolbar: {
+            show: true,
+        },
+    },
+
+    series: [50, 30, 20],
+
+    labels: ['Cpp', 'Python', 'Java'],
+
+    title: {
+        text: "Most Used Language",
+        align: 'left',
+        style: {
+            fontSize: "15px",
+            color: 'black',
+        }
+    },
+
+    plotOptions: {
+        pie: {
+            donut: {
+                size: '55%',
+                labels: {
+                    show: true,
+                    name: {
+                        show: true,
+                        fontSize: "15px",
+                        fontWeight: 'bold'
+                    },
+                    value: {
+                        show: true,
+                        fontSize: "15px",
+                        fontWeight: 'bold',
+                        formatter: function (val) {
+                            return Math.round(val) + "%";
+                        }
+                    },
+                }
+            },
+        }
+    },
+
+    legend: {
+        show: true,
+        position: "bottom",
+        horizontalAlign: "center",
+        fontSize: "14px",
+    },
+
+    dataLabels: {
+        enabled: true,
+        style: {
+            fontSize: "14px",
+            fontWeight: 600
+        },
+        formatter: function (val) {
+            return Math.round(val) + "%";
+        }
+    },
+
+    tooltip: {
+        enabled: false,
+    },
+}
+
+var chartspace = document.querySelector("#languageChart");
+var chart2 = new ApexCharts(chartspace, donut);
+chart2.render();
