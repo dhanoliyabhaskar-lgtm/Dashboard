2c72a5b6ef6354114054109b814aac1fa89422b5
Bhaskar Dhanoliya <dhanoliyabhaskar@gmail.com>
2026-07-22 20:45:14 +0530

    Implement sidebar toggle and responsive adjustments
    Added functions to toggle sidebar visibility and handle responsive behavior.

--- static/settingstheme.js
+++ static/settingstheme.js
@@ -6,7 +6,6 @@ const currentTheme = wholebody.dataset.theme;
 
 console.log("Current Theme Value from HTML attribute:", currentTheme);
 
-
 if (currentTheme == '2') { 
     //wholebody.style.backgroundColor = 'rgb(15, 15, 18)';
     //if(box) box.style.backgroundColor = 'rgb(28, 30, 36)';
@@ -15,4 +14,30 @@ if (currentTheme == '2') {
     //wholebody.style.backgroundColor = 'rgb(219, 237, 245)';
     //if(box) box.style.backgroundColor = 'rgb(255, 255, 255)';
     //wholebody.style.color = 'rgb(0, 0, 0)';
+}
+
+
+function roggle() {
+    let sidebar = document.querySelector('.sidebar');
+    let logo = document.querySelector('.sidebar .logo');
+
+    if (sidebar.style.display != 'none') {
+        sidebar.style.display = 'none';
+    } else {
+        sidebar.style.display = 'block';
+        sidebar.style.width = '70vw';
+        sidebar.style.position = 'fixed';
+        sidebar.style.zIndex = '11';
+        sidebar.style.height = '100vh';
+        logo.style.visibility = 'hidden';
+    }
+};
+
+function moggle() {
+    if (window.innerWidth <= 1400) {
+        let sidebars = document.querySelector('.sidebar');
+        if (sidebars.style.display != 'none') {
+            sidebars.style.display = 'none';
+        }
+    }
 }
