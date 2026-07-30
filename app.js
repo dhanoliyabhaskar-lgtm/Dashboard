0ff4d66bc8ed97019af0688da5b829a8390874c7
Bhaskar <dhanoliyabhaskar@gmail.com>
2026-07-30 16:34:04 +0530

    changing the app file

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const io = new Server(server, {
     cors: { origin: "*" }
